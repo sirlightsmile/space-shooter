c5146a70941c7813965b2dcf301d1c56217bd952
Sarunkorn K <sirlight.k@gmail.com>
2021-10-11 23:45:52 +0700

    move weapon to weapon folder

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace SmileProject.SpaceShooter
+{
+	public enum AttackType
+	{
+		Laser,
+		Bullet
+	}
+
+	public class Weapon
+	{
+		protected AttackType attackType;
+		protected int durability;
+		protected int level;
+		protected int maxLevel;
+		protected int damage;
+
+		public void SetDamage(int damage)
+		{
+			this.damage = damage;
+		}
+
+		public void SetLevel(int level)
+		{
+			this.level = level;
+		}
+
+		public void SetMaxLevel(int maxLevel)
+		{
+			this.maxLevel = maxLevel;
+		}
+
+		public void SetAttackType(AttackType attackType)
+		{
+			this.attackType = attackType;
+		}
+
+		public void SetDurability(int durability)
+		{
+			this.durability = durability;
+		}
+	}
+}
---
+++ Assets/Scripts/Weapon/WeaponFactory.cs
@@ -0,0 +1,11 @@
+
+namespace SmileProject.SpaceShooter
+{
+	public class WeaponFactory
+	{
+		public T CreateWeapon<T>() where T : Weapon
+		{
+			return null;
+		}
+	}
+}
