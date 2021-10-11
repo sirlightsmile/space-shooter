719d7ce9283871aa28f3f6315cc952bd6df411b0
Sarunkorn K <sirlight.k@gmail.com>
2021-10-11 23:46:50 +0700

    add event on player space ship

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace SmileProject.SpaceShooter
 {
-	public class PlayerController : MonoBehaviour
+	public class PlayerController
 	{
 		[SerializeField]
 		private PlayerSpaceShip player;
@@ -11,15 +11,45 @@ namespace SmileProject.SpaceShooter
 		[SerializeField]
 		private PoolManager<Bullet> playerBulletPool;
 
+		private bool allowControl = true;
+
+		public void Initialize()
+		{
+			player.Destroyed += OnPlayerDestroyed;
+		}
+
+		public void SetAllowControl(bool allowControl)
+		{
+			this.allowControl = allowControl;
+		}
 
 		public void Update()
 		{
+			if (!allowControl)
+			{
+				return;
+			}
+
 			MoveDirection direction;
 			float axis = Input.GetAxisRaw("Horizontal");
 			if (Enum.TryParse<MoveDirection>(axis.ToString(), out direction))
 			{
 				player.MoveToDirection(direction);
 			}
+
+			if (Input.GetButtonDown("Fire1"))
+			{
+				//TODO: fire repeat
+			}
+			else if (Input.GetButtonUp("Fire1"))
+			{
+				//TODO: stop fire
+			}
+		}
+
+		private void OnPlayerDestroyed(System.Object sender, EventArgs args)
+		{
+			SetAllowControl(false);
 		}
 	}
 }
