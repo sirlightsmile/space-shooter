a1ade54fdcea22abfeef84d0d28646a919878060
Sarunkorn K <sirlight.k@gmail.com>
2021-10-11 20:53:56 +0700

    add base spacship class

---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SmileProject.SpaceShooter
+{
+	public abstract class SpaceShip : MonoBehaviour
+	{
+		[SerializeField]
+		protected int hp;
+
+		[SerializeField]
+		protected float speed;
+
+		[SerializeField]
+		protected float atk;
+
+		public SpaceShip(int hp, float speed, float atk)
+		{
+			this.hp = hp;
+			this.speed = speed;
+			this.atk = atk;
+		}
+
+		public abstract void GetHit();
+
+		protected abstract void OnShipDestroy();
+	}
+}
