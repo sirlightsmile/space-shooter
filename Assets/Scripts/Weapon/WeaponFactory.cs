611acb052beeee161e8df074c3b0427c6f4a3132
Sarunkorn K <sirlight.k@gmail.com>
2021-10-13 17:17:10 +0700

    implement weapon factory

--- Assets/Scripts/Weapon/WeaponFactory.cs
+++ Assets/Scripts/Weapon/WeaponFactory.cs
@@ -1,11 +1,29 @@
+using UnityEngine;
 
 namespace SmileProject.SpaceShooter
 {
 	public class WeaponFactory
 	{
-		public T CreateSpaceshipGun<T>() where T : Weapon
+		private GameDataManager gameDataManager;
+
+		public WeaponFactory(GameDataManager gameDataManager)
 		{
-			return null;
+			this.gameDataManager = gameDataManager;
+		}
+
+		public SpaceshipGun CreateSpaceshipGun(SpaceshipGunModel model)
+		{
+			SpaceshipGun gun = new SpaceshipGun(model);
+			return gun;
+		}
+
+		public SpaceshipGun CreateRandomSpaceshipGun()
+		{
+			var spaceshipGuns = this.gameDataManager.GetSpaceshipGunModels();
+			int randomIndex = Random.Range(0, spaceshipGuns.Length);
+			SpaceshipGunModel randomModel = spaceshipGuns[randomIndex];
+			SpaceshipGun randomGun = new SpaceshipGun(randomModel);
+			return randomGun;
 		}
 	}
 }
