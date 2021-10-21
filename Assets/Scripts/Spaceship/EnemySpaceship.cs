05a968723164826964f86124865b950ed0e35719
Sarunkorn K <sirlight.k@gmail.com>
2021-10-21 23:58:48 +0700

    add TODO

--- Assets/Scripts/Spaceship/EnemySpaceship.cs
+++ Assets/Scripts/Spaceship/EnemySpaceship.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using SmileProject.Generic;
-using UnityEngine;
+﻿using SmileProject.Generic;
 
 namespace SmileProject.SpaceShooter
 {
@@ -9,11 +7,14 @@ namespace SmileProject.SpaceShooter
 		private int destroyScore;
 		private SoundKeys destroyedSound;
 
+		//TODO: implement enemy spaceship attack formation
+
 		public void SetDestroyScore(int destroyScore)
 		{
 			this.destroyScore = destroyScore;
 		}
 
+		//TODO: on hit sound
 		public void SetSounds(AudioManager audioManager, SoundKeys destroyedSound)
 		{
 			this.audioManager = audioManager;
